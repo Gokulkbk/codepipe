c0cfdbf2989ae208903cce34d2a5735c9a7a7a11
Gokul K <42089605+Gokulkbk@users.noreply.github.com>
2025-08-13 19:13:23 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,4 +7,4 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-aws s3 cp s3://gokul-401646244080/app.jar /home/ubuntu/app.jar  #make sure to change the bucket name here
+aws s3 cp s3://gokul-s3-bucket-ap-south/app.jar /home/ubuntu/app.jar  #make sure to change the bucket name here
